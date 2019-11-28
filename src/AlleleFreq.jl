759732ad1f73c7ea6b9026a48b6d691d88cfafc7
Pavel <pdimens@live.com>
2019-11-28 10:11:16 -0600

    Update AlleleFreq.jl

--- src/AlleleFreq.jl
+++ src/AlleleFreq.jl
@@ -38,32 +38,6 @@ function geno_freq(locus::SubArray{Union{Missing,Tuple},1})
     return d
 end
 
-#=============================================================
-REMOVE?
-
-"""
-  allele_freq_alpha(locus::PopObj)
-Returns an array of `Dicts` of allele counts per locus
-"""
-function allele_freq_alpha(locus::PopObj)
-    y = PopOpt(locus)
-    tmp = names(y.loci)[1]  # restrict to single locus for testing
-    d = Dict()
-    for genotype in y.loci[!, tmp]
-        if genotype === missing
-            continue
-        else
-            for allele in genotype
-                d[allele] = get!(d, allele, 0) + 1
-            end
-        end
-    end
-    total = values(d) |> sum
-    [d[i] = d[i] / total for i in keys(d)]
-    return d
-end
-==============================================================#
-
 """
     allele_freq_mini(locus::Array{Union{Missing, Tuple},1})
 Calculate allele counts for a single locus of a `PopObj`. Returns a `Dict` of
