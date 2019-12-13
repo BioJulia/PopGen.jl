9f5fdd74a0914f76ef8f079e2b76b46fe5adec33
Pavel <pdimens@live.com>
2019-12-12 22:55:22 -0600

    Update AlleleFreq.jl

--- src/AlleleFreq.jl
+++ src/AlleleFreq.jl
@@ -1,11 +1,11 @@
 """
-    allele_avg(x::PopObj, rounding::Bool = true)
+    allele_avg(data::PopObj, rounding::Bool = true)
 Returns a NamedTuple of the average number of alleles ('avg') and standard
 deviation (`stdev`) of a `PopObj`. Use `false` as second argument (no keyword)
 to not round results. Default (`true`) rounds to 4 digits.
 """
-function allele_avg(x::PopObj, rounding::Bool = true)
-    all_dicts = map(allele_freq, eachcol(x.loci))
+function allele_avg(data::PopObj, rounding::Bool = true)
+    all_dicts = map(allele_freq, eachcol(data.loci))
     just_alleles = map(i -> keys(i) |> collect, all_dicts)
     num_alleles = map(length, just_alleles)
     avg = mean(num_alleles)
@@ -35,21 +35,17 @@ function allele_freq(genotype::Union{Missing,Tuple})
 end
 
 """
-    allele_freq(x::Array{Union{Missing, Tuple},1})
+    allele_freq(locus::Vector{Union{Missing, Tuple}})
 Calculate allele counts for a single locus of a `PopObj`. Returns a `Dict` of
 allele's and their frequencies.
 """
-function allele_freq(x::Array{Union{Missing,Tuple},1})
+function allele_freq(locus::Array{Union{Missing,Tuple},1})
     d = Dict()
-    for row in x
-        # sum up missing
-        if row === missing
-            continue
-        else
+    for geno in locus
+        genotype === missing && continue
         # sum up alleles
-            for allele in row
-                d[allele] = get!(d, allele, 0) + 1
-            end
+        for allele in genotype
+            d[allele] = get!(d, allele, 0) + 1
         end
     end
     total = values(d) |> sum    # sum of all non-missing alleles
@@ -59,21 +55,17 @@ end
 
 
 """
-    allele_freq(x::SubArray{Union{Missing, Tuple},1})
+    allele_freq(locus::SubArray{Union{Missing, Tuple},1})
 Calculate allele counts for a single locus of a `PopObj` split by population
 using `group()`. Returns a `Dict` of allele's and their frequencies.
 """
-function allele_freq(x::SubArray{Union{Missing,Tuple},1})
+function allele_freq(locus::SubArray{Union{Missing,Tuple},1})
     d = Dict()
-    for row in x
-        # sum up missing
-        if row === missing
-            continue
-        else
+    for genotype in locus
+        genotype === missing && continue
         # sum up alleles
-            for allele in row
-                d[allele] = get!(d, allele, 0) + 1
-            end
+        for allele in genotype
+            d[allele] = get!(d, allele, 0) + 1
         end
     end
     total = values(d) |> sum    # sum of all non-missing alleles
@@ -83,21 +75,19 @@ end
 
 
 """
-    geno_freq(x::Array{Union{Missing, Tuple},1})
+    geno_freq(locus::Vector{Union{Missing, Tuple}})
 Calculate genotype frequencies of all loci in a `PopObj`. Returns a `Dict` of
 genotypes and their frequencies.
 """
-function geno_freq(x::Array{Union{Missing,Tuple},1})
+function geno_freq(locus::Vector{Union{Missing,Tuple}})
     d = Dict()
     # conditional testing if all genos are missing
-    ismissing.(x) |> unique == [true] && return missing
-    for row in x
-        # sum up missing
-        if row === missing
-            continue
-        else
+    ismissing.(locus) |> unique == [true] && return missing
+    for genotype in locus
+        genotype === missing && continue
+
         # sum up non-missing genotypes
-            d[row] = get!(d, row, 0) + 1
+        d[genotype] = get!(d, genotype, 0) + 1
         end
     end
     total = values(d) |> sum    # sum of all non-missing genotypes
@@ -106,21 +96,19 @@ function geno_freq(x::Array{Union{Missing,Tuple},1})
 end
 
 """
-    geno_freq(x::SubArray{Union{Missing, Tuple},1})
+    geno_freq(locus::SubArray{Union{Missing, Tuple},1})
 Calculate genotype frequencies of all loci in `PopObj` split
 by population using `group()`. Returns a `Dict` of genotypes and their frequencies.
 """
-function geno_freq(x::SubArray{Union{Missing,Tuple},1})
+function geno_freq(locus::SubArray{Union{Missing,Tuple},1})
     d = Dict()
     # conditional testing if all genos are missing
-    ismissing.(x) |> unique == [true] && return missing
-    for row in x
-        # sum up missing
-        if row === missing
-            continue
-        else
+    ismissing.(locus) |> unique == [true] && return missing
+    for genotype in locus
+        genotype === missing && continue
+
         # sum up non-missing genotypes
-            d[row] = get!(d, row, 0) + 1
+        d[genotype] = get!(d, genotype, 0) + 1
         end
     end
     total = values(d) |> sum    # sum of all non-missing genotypes
@@ -129,7 +117,7 @@ function geno_freq(x::SubArray{Union{Missing,Tuple},1})
 end
 
 """
-    get_genotype(x::PopObj; sample::String, locus::String)
+    get_genotype(data::PopObj; sample::String, locus::String)
 Return the genotype of a specific sample for specific locus in a `PopObj`.
 This is a barebones variant to `isolate_genotypes` that is ~1000x faster and
 suitable for development.
@@ -137,19 +125,19 @@ suitable for development.
 `get_genotype(nancycats, sample = "N115" , locus = "fca8")`
 
 """
-function get_genotype(x::PopObj; sample::String, locus::String)
-    idx = findfirst(i -> i == sample, x.samples.name)
-    return getindex(x.loci[!, Symbol(locus)], idx)
+function get_genotype(data::PopObj; sample::String, locus::String)
+    idx = findfirst(i -> i == sample, data.samples.name)
+    return getindex(data.loci[!, Symbol(locus)], idx)
 end
 
 """
-    get_sample_genotypes(x::PopObj, sample::String)
+    get_sample_genotypes(data::PopObj, sample::String)
 Return all the genotypes of a specific sample in a `PopObj`.
 This is an extension for the internal function `get_genotype`.
 
 `get_sample_genotypes(nancycats(), "N115")`
 """
-function get_sample_genotypes(x::PopObj, sample::String)
-    idx = findfirst(i -> i == sample, x.samples.name)
-    map(i -> i[idx] , eachcol(x.loci))
+function get_sample_genotypes(data::PopObj, sample::String)
+    idx = findfirst(i -> i == sample, data.samples.name)
+    map(i -> i[idx] , eachcol(data.loci))
 end
