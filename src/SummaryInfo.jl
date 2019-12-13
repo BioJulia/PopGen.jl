6e6f0867620d89363d9ddb61268ded968993b252
jdselwyn <jdselwyn@gmail.com>
2019-12-12 22:14:05 -0600

    Create SummaryInfo.jl
    Basic allelic richness. To be built upon

---
+++ src/SummaryInfo.jl
@@ -0,0 +1,21 @@
+"""
+    richness(data::PopObj)
+Calculates various allelic richness and returns vector of per locus allelic richness. To be called internally by
+function calculating overall or per population richness either rarefied or not.
+
+"""
+function richness(data::PopObj)
+    rich = []
+    @inbounds for locus in eachcol(data.loci, false)
+        unique_genos = unique(locus)
+        unique_genos = collect(skipmissing(unique_genos))
+
+        unique_alleles = vec(reduce(hcat, getindex.(unique_genos,i) for i in eachindex(x[1]))) |> unique
+
+        rich_locus = length(unique_alleles)
+        push!(rich, rich_locus)
+    end
+    return rich
+end
+
+# gulfsharks() |> richness
