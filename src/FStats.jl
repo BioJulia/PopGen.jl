420347d0f0d1ffefa566792945c06a481f8d47a7
jdselwyn <jdselwyn@gmail.com>
2019-12-22 14:57:05 -0600

    add permutation test

--- src/FStats.jl
+++ src/FStats.jl
@@ -132,3 +132,19 @@ function fst_sample(data::PopObj)
         FIT = round.(FIT, digits = 4),
     )
 end
+
+"""
+    f_stat_p(data::PopObj, nperm::Int64)
+Performs a permutation test (permuting individuals among populations) to calculate p-values for f-statistics
+"""
+function f_stat_p(data::PopObj, nperm::Int64 = 1000)
+    tmp = deepcopy(data)
+
+    observed = f_stats(data, "global")
+    sims = deepcopy(observed)
+    for boot in 1:(nperm-1)
+        shuffle!(tmp.samples.population)
+        append!(sims, f_stats(tmp, "global"))
+    end
+    return map(i -> sum((observed .<= sims)[!,i])/nperm, 1:ncol(observed))
+end
