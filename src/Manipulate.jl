d33197edaaa1b8fc017a9af8546d255edfcc01ba
Pavel V. Dimens <pdimens@live.com>
2020-02-22 23:55:36 -0600

    update: locations, loci, meta, missing

--- src/Manipulate.jl
+++ src/Manipulate.jl
@@ -12,25 +12,33 @@ derived from the PopData. Changes made to this table will not alter the source
 Use `locations!` to add spatial data to a `PopData` object.
 """
 function locations(data::PopData)
-    select(data.samples, (:longitude, :latitude))
+    select(data.meta, (:longitude, :latitude))
 end
 
 
 """
     locations!(data::PopData; lat::Vector{Float64}, long::Vector{Float64})
 Replaces existing `PopData` location data (latitude `lat`, longitude `long`).
-Takes decimal degrees as a `Vector` of any `AbstractFloat`. **Must** use negative
-sign `-` instead of cardinal directions. Location data must be in the order that
-samples appear in your `PopData`.
+Takes decimal degrees as a `Vector` of any `AbstractFloat`.
+## Formatting requirements
 - Decimal Degrees format: `-11.431`
+- **Must** use negative sign `-` instead of cardinal directions
+- Location data must be in the order that samples appear in your `PopData`
+- Missing data should be coded as `missing` values of type `Missing` (can be accomplished with `replace!()`)
+### Example
+```
+ncats = nancycats() ;
+x = rand(237) ; y = rand(237)
+locations!(ncats, long = x, lat = y)
+```
 """
-function locations!(data::PopData; lat::Vector{T}, long::Vector{T}) where T <: AbstractFloat
-    length(lat) != length(long) && error("latitude ($(length(lat))) and longitude $(length(long)) arrays not equal in length")
-    length(lat) != length(data.samples.columns.name) && error("lat/long array length ($(length(lat))) and number of samples in PopData $(length(long)) are not equal")
+function locations!(data::PopData, lat::Vector{Union{Missing,T}}, long::Vector{Union{Missing,T}}) where T <: AbstractFloat
+    length(lat) != length(long) && error("latitude ($(length(lat))) and longitude ($(length(long))) arrays not equal in length")
+    length(lat) != length(data.meta.columns.name) && error("lat/long array length ($(length(lat))) and number of samples in PopData $(length(long)) are not equal")
 
     for i in 1:length(lat)
-        data.samples.columns.longitude[i] = lat[i]
-        data.samples.columns.latitude[i] = long[i]
+        data.meta.columns.longitude[i] = lat[i]
+        data.meta.columns.latitude[i] = long[i]
     end
 end
 
@@ -38,20 +46,28 @@ end
 """
     locations!(data::PopData; lat::Vector{String}, long::Vector{String})
 Replaces existing `PopData` location data (latitude `lat`, longitude `long`). Takes
-decimal minutes format as a `Vector` of `String`. **Must** use negative sign `-`
-instead of cardinal directions. Location data must be in the order that samples
-appear in your `PopData`. Recommended to use `CSV.read` from `CSV.jl` to import
-your spatial coordinates from a text file.
+decimal minutes format as a `Vector` of `String`. Recommended to use `CSV.read`
+from `CSV.jl` to import your spatial coordinates from a text file.
+## Formatting requirements
 - Decimal Minutes: `"-11 43.11"` (must use space and be a `String`)
+- **Must** use negative sign `-` instead of cardinal directions
+- Location data must be in the order that samples appear in your `PopData`
+- Missing data should be coded as `missing` values of type `Missing` (can be accomplished with `replace!()`)
+### Example
+```
+ncats = nancycats();
+x = fill("-11 22.33", 237) ; y = fill("-41 31.52", 237)
+locations!(ncats, long = x, lat = y)
+```
 """
-function locations!(data::PopData; lat::Vector{String}, long::Vector{String})
+function locations!(data::PopData, lat::Vector{Union{Missing,String}}, long::Vector{Union{Missing,String}})
     length(lat) != length(long) && error("latitude ($(length(lat))) and longitude $(length(long)) arrays not equal in length")
-    length(lat) != length(data.samples.columns.name) && error("lat/long array length ($(length(lat))) and number of samples in PopData $(length(long)) are not equal")
+    length(lat) != length(data.meta.columns.name) && error("lat/long array length ($(length(lat))) and number of samples in PopData $(length(long)) are not equal")
     @info "Converting decimal minutes to decimal degrees"
-    # convert lat to decimal degrees
-    latConverted = Vector{Float32}()
-    longConverted = Vector{Float32}()
-    for idx in 1:length(lat)
+    # convert coordinates to decimal degrees
+    latConverted = Vector{Union{Missing,Float32}}()
+    longConverted = Vector{Union{Missing,Float32}}()
+    @inbounds for idx in 1:length(lat)
         tmpLat = split(lat[idx], " ")
         tmpLong = split(long[idx], " ")
         lat_deg = parse(Float32,tmpLat[1]) ; lat_min = round(parse(Float32,tmpLat[2])/60, digits = 4)
@@ -72,39 +88,19 @@ function locations!(data::PopData; lat::Vector{String}, long::Vector{String})
         end
     end
     for i in 1:length(latConverted)
-        data.samples.columns.longitude[i] = latConverted[i]
-        data.samples.columns.latitude[i] = longConverted[i]
+        data.meta.columns.longitude[i] = latConverted[i]
+        data.meta.columns.latitude[i] = longConverted[i]
     end
 end
 
-"""
-    locations!(data::PopData, convert::Bool = true; kwargs...)
-Replaces existing `PopData` location data (latitude, longitude). Requires all four
-keyword arguments. Takes decimal minutes format as vectors of degrees (`Int`) and
-decimal minutes (`Float`) **Must** use negative sign `-` instead of cardinal directions.
-Location data must be in the order that samples appear in your `PopData`. Recommended
-to use `CSV.read` from `CSV.jl` to import your spatial coordinates from a text file.
-### Keyword Arguments:
-- `lat_deg::Vector{Int}` a vector of postive or negative integers denoting the latitude degrees
-    - example: `[11, -12, 15, 11]`
-- `lat_min::Vector{Float64}` a vector of positive floating point numbers denoting the latitude decimal minutes
-    - example: `[43.12, 41.32, 36.53, 22.41]`
-- `long_deg::Vector{Int}` same as `lat_deg` but for longitude degrees
-- `long_min::Vector{Float64}` same as `lat_min` but for longitude minutes
 
-### Example
-If you have decimal-minutes coordinates for two samples:
-- Sample 1  _Long:_ 11 43.12  _Lat:_ 15 36.53
-- Sample 2  _Long:_ -12 41.32 _Lat:_ 11 22.41
-\n then your inputs would be:
-```
-long_deg = [11, -12]
-long_min = [43.12, 41.32]
-lat_deg  = [15, 11]
-lat_min  = [36.53, 22.41]
-```
-"""
-function locations!(data::PopData, split::Bool = true; lat_deg::Vector{Int}, long_deg::Vector{Int}, lat_min::Vector{Float64}, long_min::Vector{Float64})
+function locations!(
+    data::PopData,
+    lat_deg::Vector{Union{Missing,Int}},
+    lat_min::Vector{Union{Missing,Float64}},
+    long_deg::Vector{Union{Missing,Int}},
+    long_min::Vector{Union{Missing,Float64}},
+)
     if any(length(lat_deg) .!= length.([long_deg, long_min, lat_min]))
         error("input array lengths do not match each other\n  lat_deg: $(length(lat_deg))\n  lat_min: $(length(lat_min))\n  long_deg: $(length(long_deg))\n  long_min: $(length(long_min))")
     elseif length(lat_deg) != length(data.meta.columns.name)
@@ -112,11 +108,15 @@ function locations!(data::PopData, split::Bool = true; lat_deg::Vector{Int}, lon
     end
 
     @info "Converting decimal minutes to decimal degrees"
-    # convert lat to decimal degrees
-    latConverted = Vector{Float32}()
-    longConverted = Vector{Float32}()
-    for i in 1:length(lat_deg)
-        if lat_deg[i] < 0
+    # convert coordinates to decimal degrees
+    latConverted = Vector{Union{Missing,Float32}}()
+    longConverted = Vector{Union{Missing,Float32}}()
+    @inbounds for i = 1:length(lat_deg)
+        if lat_deg[i] === missing
+            push!(latConverted, missing)
+            push!(longConverted, missing)
+            continue
+        elseif lat_deg[i] < 0
             # if negative, subtract
             push!(latConverted, lat_deg[i] - lat_min[i])
         else
@@ -132,15 +132,72 @@ function locations!(data::PopData, split::Bool = true; lat_deg::Vector{Int}, lon
         end
     end
 
-    for i in 1:length(latConverted)
-        data.samples.columns.longitude[i] = latConverted[i]
-        data.samples.columns.latitude[i] = longConverted[i]
+    @inbounds for i = 1:length(latConverted)
+        data.meta.columns.longitude[i] = latConverted[i]
+        data.meta.columns.latitude[i] = longConverted[i]
     end
 end
 
+
+"""
+    locations!(data::PopData; kwargs...)
+Replaces existing `PopData` location data (latitude, longitude). Requires all four
+keyword arguments. Takes decimal minutes format as vectors of degrees (`Int`) and
+decimal minutes (`Float`). Recommended
+to use `CSV.read` from `CSV.jl` to import your spatial coordinates from a text file.
+### Keyword Arguments:
+- `lat_deg::Vector{Int}` a vector of postive or negative integers denoting the latitude degrees
+    - example: `[11, -12, 15, 11]`
+- `lat_min::Vector{Float64}` a vector of positive floating point numbers denoting the latitude decimal minutes
+    - example: `[43.12, 41.32, 36.53, 22.41]`
+- `long_deg::Vector{Int}` same as `lat_deg` but for longitude degrees
+- `long_min::Vector{Float64}` same as `lat_min` but for longitude minutes
+
+## Formatting requirements
+- **Must** use negative sign `-` instead of cardinal directions
+- Location data must be in the order that samples appear in your `PopData`
+- Missing data should be coded as `missing` values of type `Missing` (can be accomplished with `replace!()`)
+
+### Example
+If you have decimal-minutes coordinates for two samples:
+- Sample 1  _Long:_ 11 43.12  _Lat:_ 15 36.53
+- Sample 2  _Long:_ -12 41.32 _Lat:_ 11 22.41
+\n then your inputs would be:
+```
+lo_deg = [11, -12]
+lo_min = [43.12, 41.32]
+la_deg  = [15, 11]
+la_min  = [36.53, 22.41]
+locations!(data, long_deg = lo_deg, long_min = lo_min, lat_deg = la_deg, lat_min = la_min)
+```
+"""
+function locations!(data::PopData; kwargs...)
+    kwargs = Dict(kwargs)
+    # vectors as strings mode
+    if any([haskey(kwargs, :lat), haskey(kwargs, :long)])
+        # check for matching lat and long keywords
+        if all([haskey(kwargs, :lat), haskey(kwargs, :long)])
+            locations!(data, kwargs[:lat], kwargs[:long])
+        else
+            error("keyword arguments \"lat\" and \"long\" must be supplied together")
+        end
+    end
+
+    # split-numeric vector mode
+    if any([haskey(kwargs, :lat_deg), haskey(kwargs, :lat_min), haskey(kwargs, :long_deg), haskey(kwargs, :long_min)])
+        # check for matching lat/long deg/min
+        if all([haskey(kwargs, :lat_deg), haskey(kwargs, :lat_min), haskey(kwargs, :long_deg), haskey(kwargs, :long_min)])
+            locations!(data, kwargs[:lat_deg], kwargs[:lat_min], kwargs[:long_deg], kwargs[:long_min])
+        else
+            error("keyword arguments \"lat_deg\", \"lat_min\", \"long_deg\", and \"long_min\" must be supplied together")
+        end
+    end
+end
+
+
 """
     loci(data::PopData)
-Returns an array of strings of the loci names in a `PopData`
+Returns an array of strings of the loci names in a `PopData` object.
 """
 function loci(data::PopData)
     levels(data.loci.columns.locus)
@@ -149,10 +206,10 @@ end
 """
     loci(data::IndexedTable)
 Convenience wrapper to return an array of column names as string in the `loci`
-Table of a `PopData`
+Table of a `PopData` object.
 """
 function loci(data::IndexedTable)
-    levels(data.loci.columns.locus)
+    levels(data.columns.locus)
 end
 
 """
@@ -171,8 +228,13 @@ function locus(data::PopData, locus::String)
     end
 end
 
+function meta(data::PopData)
+    data.meta
+end
+
+const metadata = meta
+
 #### Find missing ####
-#TODO
 """
     missing(data::PopData; mode::String = "sample")
 Get missing genotype information in a `PopData`. Specify a mode of operation
@@ -189,66 +251,22 @@ to return a DataFrame corresponding with that missing information.
 missing(gulfsharks(), mode = "pop")
 ```
 """
-function Base.missing(data::PopData; mode::String = "sample")
+@inline function Base.missing(data::PopData; mode::String = "sample")
     if mode == "sample" || mode == "individual"
-        # missing per sample
-        ind_geno = map(i -> get_sample_genotypes(data, i), data.samples.name)
-        count_miss_ind = map(ind_geno) do ind
-            count(i -> i === missing, ind)
-        end
 
-        # which loci are missing per sample
-        loci_names = loci(data)
-        miss_loci = map(ind_geno) do ind
-            [loci_names[i] for i in findall(j -> j === missing, ind)]
-        end
-
-    by_sample_df = DataFrame(
-        name = data.samples.name,
-        missing = count_miss_ind,
-        loci = miss_loci |> Vector{Vector{String}}
-    )
-
-    return by_sample_df
+        return @groupby data.loci :name {missing = sum(ismissing.(:genotype))}
 
     elseif mode == "pop" || mode == "population"
-        # missing per sample
-        ind_geno = map(i -> get_sample_genotypes(data, i), data.samples.name)
-        count_miss_ind = map(ind_geno) do ind
-            count(i -> i === missing, ind)
-        end
 
-        by_sample_df = DataFrame(
-            name = data.samples.name,
-            population = data.samples.population,
-            missing = count_miss_ind
-        )
-
-        # collapse by_sample_df to get missing per population
-        by_pop_df = by(by_sample_df, :population, missing = :missing => sum)
-
-        return by_pop_df
+        return @groupby data.loci :population {missing = sum(ismissing.(:genotype))}
 
     elseif mode == "locus" || mode == "loci"
-        # missing per locus
-        miss_per_loci = map(eachcol(data.loci)) do col
-            count(i->i===missing, col)
-        end
 
-        by_locus_df = DataFrame(
-            locus = loci(data),
-            missing = miss_per_loci
-        )
-
-        return by_locus_df
+        return @groupby data.loci :locus {missing = sum(ismissing.(:genotype))}
 
     elseif mode == "detailed" || mode == "full"
-        y = deepcopy(data.loci)
-        insertcols!(y, 1, :population => data.samples.population)
 
-        # get missing per locus per pop
-        z = aggregate(y, :population, i -> count(j -> j === missing, i))
-        return rename!(z, [:population, names(data.loci)...])
+        return @groupby data.loci (:locus, :population) {missing = sum(ismissing.(:genotype))}
 
     else
         @error "Mode \"$mode\" not recognized. Please specify one of: sample, pop, locus, or full"
@@ -262,15 +280,15 @@ View unique population ID's and their counts in a `PopData`.
 
 - `listall = true` displays all samples and their `population` instead (default = `false`)
 """
-function populations(data::PopData; listall::Bool = false)
+@inline function populations(data::PopData; listall::Bool = false)
     if listall == true
-        return select(data.samples, (:name, :population))
+        return select(data.meta, (:name, :population))
     else
-        if all(ismissing.(data.samples.columns.population)) == true
+        if all(ismissing.(data.meta.columns.population)) == true
             @info "no population data present in PopData"
             return populations(data, listall = true)
         end
-        return @groupby data.samples :population {count = length(:population)}
+        return @groupby data.meta :population {count = length(:population)}
     end
 end
 
@@ -406,27 +424,32 @@ const population = populations
 const population! = populations!
 const popnames! = populations!
 
-##### Removal #####
-
+##### Exclusion #####
+#=
+To have a built-in "undo button", exclusion functions return new PopData objects
+with the specified loci/samples removed rather than overwriting the original.
+=#
+#TODO
 """
 ```
-remove_loci!(data::PopData, locus::String)
-remove_loci!(data::PopData, loci::Vector{String})
+exclude_loci(data::PopData, locus::String)
+exclude_loci(data::PopData, loci::Vector{String})
 ```
-Removes selected loci from a `PopData`.
+Exclude selected loci from a `PopData` object. Returns a new `PopData` object,
+leaving the original intact. Synonymous with `omit_loci` and `remove_loci`.
 
 ### Examples
 ```
-remove_loci!(nancycats(), "fca8")
-remove_loci!(nancycats(), ["fca8", "fca23"])
+exclude_loci(nancycats(), "fca8")
+exclude_loci!(nancycats(), ["fca8", "fca23"])
 ```
 """
-function remove_loci!(data::PopData, locus::String)
+function exclude_loci(data::PopData, locus::String)
     locus ∉ loci(data) && error("Locus \"$locus\" not found")
     data.loci = @where data.loci :locus != locus
 end
 
-function remove_loci!(data::PopData, loci::Vector{String})
+function exclude_loci(data::PopData, loci::Vector{String})
     for each in loci
         if each ∉ loci(data)
             println("NOTICE: locus \"$each\" not found")
@@ -434,17 +457,21 @@ function remove_loci!(data::PopData, loci::Vector{String})
     data.loci =  @where data.loci :locus ∉ loci
 end
 
+const omit_loci = exclude_loci
+const remove_loci = exclude_loci
+
 """
-    remove_samples!(data::PopData, samp_id::Union{Vector{String}})
-Removes selected samples from a `PopData`.
+    exclude_samples!(data::PopData, samp_id::Union{Vector{String}})
+Exclude selected samples from a `PopData` object. Returns a new `PopData` object,
+leaving the original intact. Synonymous with `omit_samples` and `remove_samples`.
 
 ### Examples
 ```
-remove_samples!(nancycats, "N100")
-remove_samples!(nancycats, ["N100", "N102", "N211"])
+exclude_samples(nancycats, "N100")
+exclude_samples(nancycats, ["N100", "N102", "N211"])
 ```
 """
-function remove_samples!(data::PopData, samp_id::Union{String, Vector{String}})
+function exclude_samples(data::PopData, samp_id::Union{String, Vector{String}})
     # get samp_id indices
     if typeof(samp_id) == String
         samp_id ∉ data.samples.name && error("sample \"$samp_id\" not found")
--- src/PopObj.jl
+++ src/PopObj.jl
@@ -5,24 +5,24 @@ Generic AbstractType for use in PopGen.jl
 abstract type PopObj end
 
 """
-    PopObj(samples::IndexedTable, loci::IndexedTable)
+    PopData(meta::IndexedTable, loci::IndexedTable)
 The data struct used for the PopGen population genetics ecosystem. You are
 STRONGLY discouraged from manually creating tables to pass into a PopObj,
 and instead should use the provided genepop, csv, or vcf file importers.
 
-- `samples` ::IndexedTable individual/sample data with the columns:
+- `meta` ::IndexedTable individual/sample data with the columns:
     - `name` ::String the individual/sample names
     - `population` ::String population names
     - `ploidy` ::Int8 ploidy in order of `ind`
     - `longitude` ::Float64 longitude values
     - `latitude` ::Float64 latitude values
 - `loci` ::IndexedTable Long-format table of sample genotype records
-    - name` ::String the individual/sample names
-    - `population`::String population name
-    - `locus` ::String of locus name
-    - `genotype` Tuple of Int8 or Int16
+    - name` ::CategoricalString the individual/sample names
+    - `population`::CategoricalString population name
+    - `locus` ::CategoricalString of locus name
+    - `genotype` Tuple of Int8 or Int16 depending on SNP or microsatellite
 """
-mutable struct PopData <: PopObj
+struct PopData <: PopObj
     meta::IndexedTable
     loci::IndexedTable
 end
