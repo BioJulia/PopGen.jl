761b49a62f8ca3ca057392a037da3afdb35a5268
Pavel Dimens <pdimens@live.com>
2020-06-08 15:34:32 -0500

    begin rewriting permutation test for fstats

--- src/FStats.jl
+++ src/FStats.jl
@@ -3,14 +3,15 @@
 Performs a permutation test (permuting individuals among populations) to calculate
 p-values for F-statistics
 """
-function f_stat_p(data::PopData, nperm::Int64 = 1000)
+function f_stat_p(data::PopData, nperm::Int = 1000)
     tmp = deepcopy(data)
-
-    observed = f_stats(data, "global")
+    observed = summary(data, by = "global")
     sims = deepcopy(observed)
-    for boot in 1:(nperm-1)
-        shuffle!(tmp.samples.population)
-        append!(sims, f_stats(tmp, "global"))
+    for permutation in 1:(nperm-1)
+        permute_samples!(tmp)
+        summary(tmp, by = "global")
+        #append!(sims, summary(tmp, by = "global"))
     end
+    return
     return map(i -> sum((observed .<= sims)[!,i])/nperm, 1:ncol(observed))
 end
