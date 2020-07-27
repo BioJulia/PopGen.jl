73205e36be8aaa35ce6c7c255f11145ee85b1f6a
Pavel Dimens <pdimens@live.com>
2020-07-27 13:22:33 -0500

    set snp thresh to 100

--- src/io/ioUtils.jl
+++ src/io/ioUtils.jl
@@ -7,9 +7,9 @@ of various file formats.
     determine_marker(geno_parse::T, digits::Int) where T<:AbstractDataFrame
 Return either `Int8` or `Int16` depending on largest allelic value in all genotypes
 in the first 10 samples of an input DataFrame (or all the samples if less than 10 samples).
-If the largest allele is 11 or greater, the marker will be considered a Microsatellite
+If the largest allele is 20 or greater, the marker will be considered a Microsatellite
 and coded in `PopData` as `Int16`, and the opposite is true for SNPs. There's no
-specific reason 10 was chosen other than it being a reasonable buffer for edge
+specific reason 100 was chosen other than it being a reasonable buffer for edge
 cases since SNP data <= 4, and haplotyped data could be a bit higher. Even if the
 microsatellite markers are coded incorrectly, there will be zero impact to performance,
 and considering how few microsatellite markers are used in typical studies, the
@@ -25,7 +25,7 @@ function determine_marker(geno_parse::T, digits::Int) where T<:AbstractDataFrame
         num_test_loc = total_col - 2
     end
     # remove everything else
-    test_df = @view geno_parse[!, 3:num_test_loc]
+    test_df = @view geno_parse[:, 3:num_test_loc]
 
     # isolate the largest allele value
     max_allele = map(eachcol(test_df)) do i
@@ -33,7 +33,7 @@ function determine_marker(geno_parse::T, digits::Int) where T<:AbstractDataFrame
         skipmissing |> Base.Iterators.flatten |> maximum
     end |> maximum
 
-    if max_allele <= 10
+    if max_allele <= 100
         return Int8
     else
         return Int16
