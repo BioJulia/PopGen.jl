c18b8396d33206106b80a5768fd320ce8e031779
jdselwyn <jdselwyn@gmail.com>
2020-07-30 18:10:56 -0500

    Update RelatednessLikelihood.jl

--- src/Relatedness/RelatednessLikelihood.jl
+++ src/Relatedness/RelatednessLikelihood.jl
@@ -148,3 +148,46 @@ function dyadicLikelihood(ind1::T, ind2::T, locus_names::Vector{Symbol}, alleles
     θ = Δ[1][1] + 0.5 * (Δ[1][3] + Δ[1][5] + Δ[1][7]) + 0.25 * Δ[1][8]
     return 2 * θ#, Δ[1], Δ[2] #secondary outputs for convergence & Δvalues if we want
 end
+
+
+
+#### No inbreeding assumption
+function Δ_optim_noInbreeding(Pr_L_S::Array{Float64,2}, verbose::Bool = false)
+    #Δ is what needs to be optimized
+    #consist of 9 values between 0 and 1 which must also sum to 1
+    #is then used to calculate relatedness
+
+    Δ = Variable(3)
+    problem = maximize(sum(log(Pr_L_S * Δ)))
+    problem.constraints += 0 <= Δ[1:3]
+    problem.constraints += Δ[1:3] <= 1
+    problem.constraints += sum(Δ) <= 1
+    problem.constraints += 0 <= sum(Δ)
+    problem.constraints += 2 * (0.5 * Δ[1] + 0.25 * Δ[2]) <= 1
+    problem.constraints += 0 <= 2 * (0.5 * Δ[1] + 0.25 * Δ[2])
+
+    Convex.solve!(problem, ECOS.Optimizer(verbose = verbose, maxit=100), verbose = verbose) #maxit=100,
+    #Convex.solve!(problem, ECOSSolver(verbose = verbose, maxit=100, feastol=5e-6, reltol = 1e-3, reltol_inacc = 5e-2), verbose = verbose)
+    #Convex.solve!(problem, SCSSolver(verbose = verbose, max_iters = 100), verbose = verbose)
+
+    Δ.value, problem.status
+    # Should probably include some output that confirms that it did in fact
+    # converge and/or use multiple random starts to confirm not a local maxima
+end
+
+function dyadicLikelihood_noInbreeding(ind1::T, ind2::T, locus_names::Vector{Symbol}, alleles::U; kwargs...) where T <: GenoArray where U <: NamedTuple
+    #TODO Add inbreeding toggle
+    Pr_Ls = Array{Float64}(undef, length(locus_names), 9)
+
+    idx = 0
+    for (loc,gen1,gen2) in zip(locus_names, ind1, ind2)
+        idx += 1
+        Pr_Ls[idx, :] = probability_state_table(gen1, gen2, allele_frequencies[loc])
+    end
+    Pr_Ls = Pr_Ls[:, 7:9]
+
+    Δ = Δ_optim_noInbreeding(Pr_Ls)
+
+    θ = (0.5 * Δ[1][1] + 0.25 * Δ[1][2])
+    return 2 * θ#, Δ[1], Δ[2] #secondary outputs for convergence & Δvalues if we want
+end
