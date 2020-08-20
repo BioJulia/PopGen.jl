dbbfec1c7cc611ae583664d6cbd6b02515407403
Pavel Dimens <pdimens@live.com>
2020-08-20 15:23:01 -0500

    remove dev code and fix docstring

--- src/Simulations.jl
+++ src/Simulations.jl
@@ -27,17 +27,7 @@ PopData Object
   │ 2    │ sim_2    │ 1          │ 2      │ missing   │ missing  │
   │ 3    │ sim_3    │ 1          │ 2      │ missing   │ missing  │
   │ 4    │ sim_4    │ 1          │ 2      │ missing   │ missing  │
-  │ 5    │ sim_5    │ 1          │ 2      │ missing   │ missing  │
-  │ 6    │ sim_6    │ 1          │ 2      │ missing   │ missing  │
-  │ 7    │ sim_7    │ 1          │ 2      │ missing   │ missing  │
-  │ 8    │ sim_8    │ 1          │ 2      │ missing   │ missing  │
-  │ 9    │ sim_9    │ 1          │ 2      │ missing   │ missing  │
   ⋮
-  │ 1691 │ sim_1691 │ 17         │ 2      │ missing   │ missing  │
-  │ 1692 │ sim_1692 │ 17         │ 2      │ missing   │ missing  │
-  │ 1693 │ sim_1693 │ 17         │ 2      │ missing   │ missing  │
-  │ 1694 │ sim_1694 │ 17         │ 2      │ missing   │ missing  │
-  │ 1695 │ sim_1695 │ 17         │ 2      │ missing   │ missing  │
   │ 1696 │ sim_1696 │ 17         │ 2      │ missing   │ missing  │
   │ 1697 │ sim_1697 │ 17         │ 2      │ missing   │ missing  │
   │ 1698 │ sim_1698 │ 17         │ 2      │ missing   │ missing  │
@@ -53,18 +43,7 @@ PopData Object
   │ 2     │ sim_1    │ 1          │ fca23  │ (132, 140) │
   │ 3     │ sim_1    │ 1          │ fca43  │ (139, 139) │
   │ 4     │ sim_1    │ 1          │ fca45  │ (126, 126) │
-  │ 5     │ sim_1    │ 1          │ fca77  │ (150, 152) │
-  │ 6     │ sim_1    │ 1          │ fca78  │ (142, 142) │
-  │ 7     │ sim_1    │ 1          │ fca90  │ (197, 199) │
-  │ 8     │ sim_1    │ 1          │ fca96  │ (91, 105)  │
-  │ 9     │ sim_1    │ 1          │ fca37  │ (208, 208) │
   ⋮
-  │ 15291 │ sim_1699 │ 17         │ fca37  │ (208, 208) │
-  │ 15292 │ sim_1700 │ 17         │ fca8   │ (133, 135) │
-  │ 15293 │ sim_1700 │ 17         │ fca23  │ (136, 146) │
-  │ 15294 │ sim_1700 │ 17         │ fca43  │ (139, 141) │
-  │ 15295 │ sim_1700 │ 17         │ fca45  │ missing    │
-  │ 15296 │ sim_1700 │ 17         │ fca77  │ (150, 156) │
   │ 15297 │ sim_1700 │ 17         │ fca78  │ (142, 142) │
   │ 15298 │ sim_1700 │ 17         │ fca90  │ (199, 199) │
   │ 15299 │ sim_1700 │ 17         │ fca96  │ (113, 113) │
@@ -90,7 +69,6 @@ function simulate(data::PopData; n::Int = 100)
         gdf,
         :genotype => allele_freq => :frq
     )
-    return freqs
     # create new genotypes
     transform!(freqs, :frq => (i -> sample_locus.(i,n,ploidy)) => :frq)
     
@@ -131,21 +109,17 @@ d = Dict(
   137 => 0.0625
   )
 
-julia> sample_locus(d, 5, 2)
+julia> sample_locus(d, 3, 2)
 5-element Array{Tuple{Int16,Int16},1}:
  (133, 135)
  (135, 135)
  (143, 137)
- (135, 135)
- (133, 133)
 
-julia> sample_locus(zz, 5, 3)
+julia> sample_locus(d, 3, 3)
 5-element Array{Tuple{Int16,Int16,Int16},1}:
  (135, 135, 133)
  (143, 135, 133)
  (137, 135, 135)
- (135, 133, 135)
- (137, 143, 135)
 ```
 """
 function sample_locus(locus::Dict, n::Int, ploidy::Signed)
