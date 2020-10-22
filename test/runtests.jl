1519e6d28fccca657b72f31aa0f8d37b5fbf466f
Pavel Dimens <pdimens@live.com>
2020-10-21 21:59:07 -0500

    add first tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,32 @@
+fatalerrors = length(ARGS) > 0 && ARGS[1] == "-f"
+quiet = length(ARGS) > 0 && ARGS[1] == "-q"
+anyerrors = false
+
 using PopGen
 using Test
 
-@testset "PopGen.jl" begin
-    # Write your own tests here.
+all_tests = [
+    "types.jl"
+]
+
+println("Running tests:")
+
+for a_test in all_tests
+    try
+        include(a_test)
+        println("\t\033[1m\033[32mPASSED\033[0m: $(a_test)")
+    catch e
+        global anyerrors = true
+        println("\t\033[1m\033[31mFAILED\033[0m: $(a_test)")
+        if fatalerrors
+            rethrow(e)
+        elseif !quiet
+            showerror(stdout, e, backtrace())
+            println()
+        end
+    end
+end
+
+if anyerrors
+    throw("Tests failed")
 end
