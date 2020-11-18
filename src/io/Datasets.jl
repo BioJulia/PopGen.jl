5466ef8fc79fec8d9e40ad6625adb764bf892be2
Pavel Dimens <pdimens@live.com>
2020-11-18 12:18:11 -0600

    add @nancycats @gulfsharks, depwarns on old

--- src/io/Datasets.jl
+++ src/io/Datasets.jl
@@ -1,5 +1,5 @@
 ## test data available for use in PopGen
-export dataset, nancycats, gulfsharks
+export dataset, nancycats, @nancycats, gulfsharks, @gulfsharks
 
 """
     dataset(::String)
@@ -19,23 +19,31 @@ function dataset(name::String)
 end
 
 """
-    nancycats()
+    @nancycats
 Returns a `PopObj` of corresponding "nancycats" dataset as featured in
 the R package `adegenet`. This is microsatellite data of 9 loci in 237
 individuals across 17 populations.
 
 Example:
 ```
-ncats = nancycats()
+ncats = @nancycats
 ```
 """
 function nancycats()
+    @warn "This method is deprecated, please use @nancycats instead"  * "\nnancycats() will be removed in the next release"
     filename = normpath(joinpath(@__DIR__,"../..","data", "datasets.jld2"))
     load(filename, "nancycats")
 end
 
+macro nancycats()
+    return esc(quote
+       nancycats()
+    end)
+end
+
+
 """
-    gulfsharks()
+    @gulfsharks
 Returns a `PopObj` corresponding the Blacknose shark dataset as used in
 Dimens et al. 2019. This is a mid-sized SNV dataset of 2209 haplotypes 
 in 212 individuals, across 7 populations.
@@ -43,10 +51,17 @@ in 212 individuals, across 7 populations.
 Example:
 
 ```
-sharks = gulfsharks()
+sharks = @gulfsharks
 ```
 """
 function gulfsharks()
+    @warn "This method is deprecated, please use @gulfsharks instead" * "\ngulfsharks() will be removed in the next release"
     filename = normpath(joinpath(@__DIR__,"../..","data", "datasets.jld2"))
     load(filename, "gulfsharks")
+end
+
+macro gulfsharks()
+    return esc(quote
+       gulfsharks()
+    end)
 end
