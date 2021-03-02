0d0c0e4bd3ab9597352fe47b4db37a7dd0c14ea6
Pavel Dimens <pdimens@live.com>
2021-03-01 23:01:38 -0600

    more accurate testset

--- test/io.jl
+++ test/io.jl
@@ -10,11 +10,13 @@ oyster_vcf = normpath(joinpath(@__DIR__,"..","data/", "filtered_oyster.vcf"))
 
 @testset "Genepop io" begin
     @test typeof(read_from(cats_gen, silent = true)) == PopData
-    @test typeof(read_from(sharks_csv, silent = true)) == PopData
     @test typeof(genepop(cats_gen, silent = true)) == PopData
-    @test typeof(delimited(sharks_csv, silent = true)) == PopData
 end
 
+@testset "delimited io" begin
+    @test typeof(read_from(sharks_csv, silent = true)) == PopData
+    @test typeof(delimited(sharks_csv, silent = true)) == PopData
+end
 
 @testset "VCF io" begin
     @test typeof(read_from(oyster_vcf, silent = true)) == PopData
