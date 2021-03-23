fe9462403fe56b54dead4afc5faea17a4b7a83a1
Pavel Dimens <pdimens@live.com>
2021-03-23 15:16:29 -0400

    add WC permutation methods

--- src/FStatistics/FstPermutations.jl
+++ src/FStatistics/FstPermutations.jl
@@ -5,25 +5,27 @@ function _permuted_WeirCockerham(data::PopData, iterations::Int64)
     n_loci = size(data)[2]
     results = zeros(npops, npops)
     perm_vector = zeros(iterations-1)
-    for i in 2:npops
-        for j in 1:(i-1)
+    p = Progress(sum(1:(npops - 1)) * (iterations - 1), dt = 1, color = :blue, barglyphs = BarGlyphs("|=> |"), barlen = 30)
+    @inbounds for i in 2:npops
+        @inbounds for j in 1:(i-1)
             pop1 = reshape(idx_pdata[i].genotype, :, n_loci)
             pop2 = reshape(idx_pdata[j].genotype, :, n_loci)
             wc_val = weircockerham_fst(pop1,pop2)
-            @sync for iter in 1:iterations-1
+            @inbounds @sync for iter in 1:iterations-1
                 Base.Threads.@spawn begin
-                    perm_p1, perm_p2 = wc_permutation(pop1,pop2)
-                    perm_vector[iter] = weircockerham_fst(perm_p1, perm_p1)
+                    perm_p1, perm_p2 = wc_permutation(pop1, pop2)
+                    perm_vector[iter] = weircockerham_fst(perm_p1, perm_p2)
+                    pops_text = string(pops[i]) * " & " * string(pops[j])
+                    ProgressMeter.next!(p; showvalues = [(:Populations, pops_text), (:Iteration, "$iter")])
                 end
             end
-            results[i,j] = wc_val
-            results[j,i] = (sum(wc_val .<= perm_vector) + 1) / iterations
+            @inbounds results[i,j] = wc_val
+            @inbounds results[j,i] = (sum(wc_val .<= perm_vector) + 1) / iterations
         end
     end
+    println("Below diagonal: FST values | Above diagonal: P values")
     return PairwiseFST(DataFrame(results, Symbol.(pops)), "Weir & Cockerham")
 end
-
-
 
 function wc_permutation(population_1::T, population_2::T) where T<:AbstractMatrix
     merged = vcat(population_1, population_2)
@@ -35,9 +37,9 @@ function wc_permutation(population_1::T, population_2::T) where T<:AbstractMatri
     # generate permutation indices for the first "population"
     perm1 = sample(Xoroshiro128Star(), 1:n_samples, p1_size, replace = false)
     # generate permutation indices for the second "population"
-    perm2 =(1:n_samples)[Not(perm1)]
+    perm2 = @inbounds (1:n_samples)[Not(perm1)]
     # index the merged matrix with the permuted indices
-    new_pop_1 = merged[perm1,:]
-    new_pop_2 = merged[perm2,:]
+    new_pop_1 = @inbounds @view merged[perm1,:]
+    new_pop_2 = @inbounds @view merged[perm2,:]
     return new_pop_1, new_pop_2 
 end
--- src/FStatistics/PairwiseFST.jl
+++ src/FStatistics/PairwiseFST.jl
@@ -22,13 +22,25 @@ end
 
 #TODO add methods and complete docstring
 """
-    pairwise_fst(data::PopData; method::String)
-Calculate pairwise FST between populations in a `PopData` object.
+    pairwise_fst(data::PopData; method::String, iterations::Int64)
+Calculate pairwise FST between populations in a `PopData` object. Set `iterations` 
+to a value greater than `0` to perform a single-tailed permutation test to obtain
+P-values of statistical significance.
 #### Methods:
 - `"Nei87"`: Nei (1987) method
 - `"WC84"` : Weir-Cockerham (1984) method (default)
+
+**Examples**
+```julia
+data = @nancycats
+wc = pairwise_fst(data, method = "WC84")
+wc_sig = pairwise_fst(data, iterations = 1000)
+```
 """
-function pairwise_fst(data::PopData; method::String = "WC")
+function pairwise_fst(data::PopData; method::String = "WC", iterations::Int64 = 0)
     occursin("nei", lowercase(method)) && return _pairwise_Nei(data)
-    occursin("wc", lowercase(method)) && return _pairwise_WeirCockerham(data)
+    if occursin("wc", lowercase(method))
+        iterations > 0 && return _permuted_WeirCockerham(data, iterations)
+        iterations == 0 && return _pairwise_WeirCockerham(data)
+    end
 end
