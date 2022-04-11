f3a7f9b76b650f06637d1244fd67ec2db0abb855
Pavel Dimens <pdimens@live.com>
2022-04-11 11:28:23 -0400

    prepend PopGen. to _allelematrix tests

--- test/allelematrices.jl
+++ test/allelematrices.jl
@@ -7,16 +7,16 @@ cats = @nancycats ;
 
 @testset "AlleleMatrices.jl" begin
     @testset "_allelematrix" begin
-        @test size(_allelematrix(cats, by = "count", missings = "mean", scale = false, center = false)) == (237, 108)
-        @test size(_allelematrix(cats, by = "frequency", missings = "missing", scale = false, center = false)) == (237, 108)
-        @test size(_allelematrix(cats, by = "frequency", missings = "zero", scale = false, center = false)) == (237, 108)
-        @test size(_allelematrix(cats, by = "frequency", missings = "mean", scale = false, center = false)) == (237, 108)
-        @test size(_allelematrix(cats, scale = true)) == (237, 108)
-        @test size(_allelematrix(cats, center = true)) == (237, 108)
-        @test _allelematrix(cats, scale = true) != _allelematrix(cats, center = true)
-        @test _allelematrix(cats, by = "count") != _allelematrix(cats)
-        @test _allelematrix(cats, missings = "missing") != _allelematrix(cats, missings = zero)
-        @test _allelematrix(cats, missings = zero) != _allelematrix(cats)
+        @test size(PopGen._allelematrix(cats, by = "count", missings = "mean", scale = false, center = false)) == (237, 108)
+        @test size(PopGen._allelematrix(cats, by = "frequency", missings = "missing", scale = false, center = false)) == (237, 108)
+        @test size(PopGen._allelematrix(cats, by = "frequency", missings = "zero", scale = false, center = false)) == (237, 108)
+        @test size(PopGen._allelematrix(cats, by = "frequency", missings = "mean", scale = false, center = false)) == (237, 108)
+        @test size(PopGen._allelematrix(cats, scale = true)) == (237, 108)
+        @test size(PopGen._allelematrix(cats, center = true)) == (237, 108)
+        @test PopGen._allelematrix(cats, scale = true) != PopGen._allelematrix(cats, center = true)
+        @test PopGen._allelematrix(cats, by = "count") != PopGen._allelematrix(cats)
+        @test PopGen._allelematrix(cats, missings = "missing") != PopGen._allelematrix(cats, missings = zero)
+        @test PopGen._allelematrix(cats, missings = zero) != PopGen._allelematrix(cats)
     end
 end
 
