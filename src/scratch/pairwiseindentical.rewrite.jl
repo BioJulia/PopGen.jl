3b70726538a0f67d09d64b7f6489ce3bef93e9ae
Pavel Dimens <pdimens@live.com>
2022-04-22 16:17:38 -0400

    minor cleanup

--- src/scratch/pairwiseindentical.rewrite.jl
+++ src/scratch/pairwiseindentical.rewrite.jl
@@ -1,18 +1,5 @@
-function pwhelper(x::T,y::U)::Float64 where T<:AbstractArray where U<:AbstractArray
-    mean(skipmissing(x .== y))
-end
 
-function pw2(data::PopData)
-    locmtx = locimatrix(data)
-    ids = unique(data.genodata.name)
-    vecs = [i for i in eachrow(locmtx)]
-    out = NamedArray(pwhelper.(vecs, permutedims(vecs)))
-    setnames!(out, String.(ids),1)
-    setnames!(out, String.(ids),2)
-    return out
-end
-
-function pwsimiarhelper(x::Int8, y::Int8)
+function pwsimilarhelper(x::Int8, y::Int8)
     (x == -1 | y == -1) && return missing
     (x == 0 & y == 0) && return false
     x == y && return true
