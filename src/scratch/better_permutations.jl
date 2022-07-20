748a2f4589bd959f81e791fa658d439edd80387b
Pavel Dimens <pdimens@live.com>
2022-07-20 11:39:48 -0400

    housekeeping

--- src/scratch/better_permutations.jl
+++
@@ -1,9 +0,0 @@
-function perm_samp(data::PopData)
-    popcounts = countmap(data.metadata.sampleinfo.population)
-    pops, counts = keys(popcounts), values(popcounts)
-    gdf = groupby(data.genodata, :name)
-    perm_idx = shuffle(Xoroshiro128Star(), keys(gdf))
-    
-    #return gdf
-
-end
